be5002fa243b8611f905b77ac23042671593213e
Edouard <edouard@voltajazz.com>
2025-05-16 16:01:07 +0200

    Fin de la séance du 15 mai (TP2 tailwind) Note : le support de cours est dispo sur myGES

--- TP2/vite.config.js
+++ TP2/vite.config.js
@@ -3,4 +3,21 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	plugins: [tailwindcss()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: ({ name }) => {
+					if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+						return "images/[name][extname]";
+					}
+					if (/\.css$/.test(name ?? "")) {
+						return "css/[name][extname]";
+					}
+					// default value
+					// ref: https://rollupjs.org/guide/en/#outputassetfilenames
+					return "assets/[name]-[hash][extname]";
+				},
+			},
+		},
+	},
 });
